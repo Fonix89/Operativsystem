ae3d1df37ecab68ff06f1c032074e04e4a4de72f
CaptMelis <081817@usn.no>
2024-03-20 11:06:39 +0100

    Complete version of Oblig2

--- oblig2.c
+++ oblig2.c
@@ -3,38 +3,74 @@
 #include <pthread.h>
 
 int balanse = 0;
-int tall = 0;
+//Mutex for å synkronisere tilgang til balanse
+pthread_mutex_t balanse_mutex = PTHREAD_MUTEX_INITIALIZER;
 
-void *start() {
-    tall = tall + 1;
-    printf("Starter tråd, med tall %d %lu\n", tall, pthread_self());
+void *lesInn(void *arg) {
+    FILE *fil = fopen("inn.txt", "r");
+    if (fil == NULL) {
+        printf("Kunne ikke åpne filen\n");
+        return NULL;
+    }
 
-    sleep(5);
-    printf("Avslutter tråd %d %lu\n", tall, pthread_self());
+    int beløp = 0;
+    while (fscanf(fil, "%d", &beløp) != EOF) {
+        pthread_mutex_lock(&balanse_mutex);
+        balanse += beløp;
+        pthread_mutex_unlock(&balanse_mutex);
+    }
+    fclose(fil);
+    return NULL;
+}
+
+void* lesUt(void *arg) {
+    FILE *fil = fopen("ut.txt", "r");
+    if (fil == NULL) {
+        printf("Kunne ikke åpne filen\n");
+        return NULL;
+    }
+
+    int beløp = 0;
+    while (fscanf(fil, "%d", &beløp) != EOF) {
+        pthread_mutex_lock(&balanse_mutex);
+        balanse -= beløp;
+        pthread_mutex_unlock(&balanse_mutex);
+    }
+    fclose(fil);
+    return NULL;
 }
 
 int main(int arch, char *argv[]) {
-    pthread_t tråd1, tråd2;
+    pthread_t tråd_inn, tråd_ut;
 
-    if(pthread_create(&tråd1, NULL, &start, NULL) != 0) {
-        printf("Kunne ikke lage tråd 1\n");
+    if(pthread_mutex_init(&balanse_mutex, NULL) != 0) {
+        printf("Mutex init feilet\n");
         return 1;
     }
 
-    if(pthread_create(&tråd2, NULL, &start, NULL) != 0) {
-        printf("Kunne ikke lage tråd 2\n");
+    if(pthread_create(&tråd_inn, NULL, &lesInn, NULL) != 0) {
+        printf("Kunne ikke lage tråd for lesInn\n");
+        return 2;
+    }
+
+    if(pthread_create(&tråd_ut, NULL, &lesUt, NULL) != 0) {
+        printf("Kunne ikke lage tråd for lesUt\n");
         return 3;
     }
 
-    if(pthread_join(tråd1, NULL) != 0) {
+    if(pthread_join(tråd_inn, NULL) != 0) {
         printf("Kunne ikke hente tråd 1\n");
         return 2;
     }
 
-    if(pthread_join(tråd2, NULL)) {
+    if(pthread_join(tråd_ut, NULL) != 0) {
         printf("Kunne ikke hente tråd 2\n");
         return 4;
     }
+
+    printf("Balansen er: %d\n", balanse);
+
+    pthread_mutex_destroy(&balanse_mutex);
 
     return 0;
 }
