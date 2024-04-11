4f66c4cb4f3291805abcefc4bb2a348cc40781ae
CaptMelis <081817@usn.no>
2024-04-11 11:27:09 +0200

    Endring med pointere og scanner som håndterer evt bokstaver

--- oblig2.c
+++ oblig2.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-#include <ctype.h>
 
 int balanse = 0;
 //Mutex for å synkronisere tilgang til balanse
@@ -10,30 +9,20 @@ pthread_mutex_t balanse_mutex = PTHREAD_MUTEX_INITIALIZER;
 void *lesInn(void *arg) {
     int* ptr_feil_kode = malloc(sizeof(int));
     *ptr_feil_kode = 0;
-
+    
     FILE *fil = fopen("inn.txt", "r");
     if (fil == NULL) {
         printf("Kunne ikke åpne filen\n");
         *ptr_feil_kode = 1;
-        return (void *) ptr_feil_kode;
+        return (void*) ptr_feil_kode;
     }
 
-    char beløp_inn[100];
-    while (fscanf(fil, "%s", &beløp_inn) != EOF) {
-        int i = 0;
-        while(beløp_inn[i] != '\0') {
-            if (isdigit(beløp_inn[i]) == 0) {
-                printf("Ugyldig input\n");
-                fclose(fil);
-                *ptr_feil_kode = 2;
-                return (void *) ptr_feil_kode;
-            }
-            i++;
-        }
+    int beløp = 0;
+    while (fscanf(fil, "%d", &beløp) != EOF) {
         pthread_mutex_lock(&balanse_mutex);
-        balanse += atoi(beløp_inn);
+        balanse += beløp;
         pthread_mutex_unlock(&balanse_mutex);
-        }
+    }
     fclose(fil);
     return (void*) ptr_feil_kode;
 }
@@ -46,25 +35,15 @@ void *lesUt(void *arg) {
     if (fil == NULL) {
         printf("Kunne ikke åpne filen\n");
         *ptr_feil_kode = 1;
-        return (void *) ptr_feil_kode;
+        return (void*) ptr_feil_kode;
     }
 
-    char beløp_ut[100];
-    while (fscanf(fil, "%s", &beløp_ut) != EOF) {
-        int i = 0;
-        while(beløp_ut[i] != '\0') {
-            if (isdigit(beløp_ut[i]) == 0) {
-                printf("Ugyldig input\n");
-                fclose(fil);
-                *ptr_feil_kode = 2;
-                return (void *) ptr_feil_kode;
-            }
-            i++;
-        }
+    int beløp = 0;
+    while (fscanf(fil, "%d", &beløp) != EOF) {
         pthread_mutex_lock(&balanse_mutex);
-        balanse -= atoi(beløp_ut);
+        balanse -= beløp;
         pthread_mutex_unlock(&balanse_mutex);
-        }
+    }
     fclose(fil);
     return (void*) ptr_feil_kode;
 }
@@ -74,24 +53,29 @@ int main(int arch, char *argv[]) {
     int* ret1 = NULL;
     int* ret2 = NULL;
 
+    if(pthread_mutex_init(&balanse_mutex, NULL) != 0) {
+        printf("Mutex init feilet\n");
+        return 1;
+    }
+
     if(pthread_create(&tråd_inn, NULL, &lesInn, NULL) != 0) {
         printf("Kunne ikke lage tråd for lesInn\n");
-        return 1;
+        return 2;
     }
 
     if(pthread_create(&tråd_ut, NULL, &lesUt, NULL) != 0) {
         printf("Kunne ikke lage tråd for lesUt\n");
-        return 2;
+        return 3;
     }
 
     if(pthread_join(tråd_inn, (void**) &ret1) != 0) {
         printf("Kunne ikke hente tråd 1\n");
-        return 3;
+        return 4;
     }
 
     if(pthread_join(tråd_ut, (void**) &ret2) != 0) {
         printf("Kunne ikke hente tråd 2\n");
-        return 4;
+        return 5;
     }
 
     printf("Balansen er: %d\n", balanse);
